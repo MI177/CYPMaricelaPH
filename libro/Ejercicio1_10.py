85543b1af4f42597570b3e21484c0cd4fcb817d7
MI177 <isassenior@gmail.com>
2019-09-14 20:06:57 -0500

    Archivo de tarea parte 1

---
+++ libro/Ejercicio1_10.py
@@ -0,0 +1,11 @@
+#Programa que cálcula el perimetro área de un rectángulo
+
+Base = int(input("Dame la base del rectángulo"))
+Altura = int(input( "Dame la altura del rectángulo"))
+
+n= Base*Altura
+n2= Base+Base+Altura+Altura
+
+print("El perimetro del triángulo es igual a", n) 
+print("El área del rectángulo es igual a ", n2) 
+
