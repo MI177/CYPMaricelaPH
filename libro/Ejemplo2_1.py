47ac70917bcdbba244fcf97a9baf49ac31022c2d
MI177 <isassenior@gmail.com>
2019-09-24 17:36:19 -0500

    Ejercicios de tarea

---
+++ libro/Ejemplo2_1.py
@@ -0,0 +1,6 @@
+#Programa 2.1 
+
+CAL = float(input("Ingresa tu calificacion" ))
+
+if (CAL>8):
+    print("Aprobaste tu calificacion es: ",CAL)
---
+++ libro/Ejemplo2_3.py
@@ -0,0 +1,8 @@
+#Programa 2.3
+
+CAL = float(input("Ingresa tu calificacion" ))
+
+if (CAL>=8):
+    print("Aprobaste tu calificacion es: ",CAL)
+else:
+    print("Reprobaste, tu calificaion es: ",CAL)
