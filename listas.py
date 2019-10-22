37e48d9db1c800cc97a87f042a3bacbf2aed04ad
MI177 <isassenior@gmail.comgit config --global user.email isassenior@gmail.com>
2019-10-22 17:40:07 -0500

    teoria

---
+++ listas.py
@@ -0,0 +1,9 @@
+#LISTAS
+
+lluvia_norte=[80,60,120,100,70,150,100,47,95,70,100,130]
+
+for indice in range (0,12,1):
+    print(f"mes {indice+1} en region norte={lluvia_norte[indice]}")
+
+print(lluvia_norte[4])
+print(lluvia_norte[:5:])
