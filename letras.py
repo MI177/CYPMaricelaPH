8c37093042f53b55f80bd0dbeeae5af145395cef
MI177 <isassenior@gmail.com>
2019-11-13 23:15:57 -0600

    Tarea

---
+++ letras.py
@@ -0,0 +1,10 @@
+import Terminal-Text-Color
+from Terminal-Text-Color import text_color
+
+
+print(default_yellow("Universidad Autonoma de México"))
+print(default_blue("FES Aragón"))
+print(default_green("Pérez Hernández Maricela Isabel"))
+
+
+
